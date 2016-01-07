5b7fa2db34ddc60529545b203140095f42d2c163
Max Presman <max@pubnub.com>
2016-01-06 22:13:50 -0800

    more nocking

--- node.js/tests/ssl_test.js
+++ node.js/tests/ssl_test.js
@@ -1,102 +1,93 @@
-var nockHarnessConfig = require("./harness/nock");
+path = require("path");
+
 
 var PUBNUB = require('../pubnub.js'),
   assert = require("assert"),
   channel = "test_javascript_ssl",
   origin = 'blah.pubnub.com',
-  uuid = "me",
+  uuid = "dd6af454-fa7a-47be-a800-1b9b050f5d94",
   message = "hello";
 
 describe("When SSL mode", function () {
+  var nockFixtureDirectory = path.resolve(__dirname, './fixtures');
+  console.log(nockFixtureDirectory);
+  var nockBackMocha = require('nock-back-mocha')(nockFixtureDirectory);
+  beforeEach(nockBackMocha.beforeEach);
+  afterEach(nockBackMocha.afterEach);
+
   describe("is enabled", function () {
-    // create a harness with the selected origin and enabled SSL
-    var nockHarness = nockHarnessConfig(origin, true, "PubNub-JS-Nodejs/3.7.18", 'demo', 'demo');
-    before(nockHarness.engage);
-    after(nockHarness.disengage);
-
-    it("should be able to successfully subscribe to the channel and publish message to it ", function (done) {
+    it("should be able to successfully subscribe to the channel and publish message to it on port 443", function (done) {
       var pubnub = PUBNUB.init({
-        publish_key: 'demo',
-        subscribe_key: 'demo',
-        ssl: true,
-        origin: origin
-      });
-
-      subscribeAndPublish(pubnub, channel + "_enabled_" + get_random(), done);
-    });
-
-    it("should send requests via HTTPS to 443 port", function (done) {
-
-      var pubnub = PUBNUB.init({
-        publish_key: 'demo',
-        subscribe_key: 'demo',
+        publish_key     : 'pub-c-82c13e30-f521-4419-b002-b8c99d5faa00',
+        subscribe_key   : 'sub-c-7756fcc2-9bd6-11e5-b0f3-02ee2ddab7fe',
         ssl: true,
         origin: origin,
         uuid: uuid
       });
 
-      nockHarness.mockPublish(channel, encodeURI('"' + message + '"'), uuid, "ssl_test__send_requests_via_https.json");
+      subscribeAndPublish(pubnub, channel + "_enabled_" + 10, done);
+    });
+
+    it("should send requests via HTTPS to 443 port", function (done) {
+      var pubnub = PUBNUB.init({
+        publish_key     : 'pub-c-82c13e30-f521-4419-b002-b8c99d5faa00',
+        subscribe_key   : 'sub-c-7756fcc2-9bd6-11e5-b0f3-02ee2ddab7fe',
+        ssl: true,
+        origin: origin,
+        uuid: uuid
+      });
 
       pubnub.publish({
         channel: channel,
         message: message,
         callback: function () {
+          pubnub.shutdown();
           done();
         },
         error: function (err) {
-          console.log(err);
+          pubnub.shutdown();
           done(new Error("Error callback triggered"));
         }
       });
     });
   });
 
-  describe.only("is disabled", function () {
-    var nockHarness = nockHarnessConfig(origin, false, "PubNub-JS-Nodejs/3.7.18");
-    before(nockHarness.engage);
-    after(nockHarness.disengage);
+  describe("is disabled", function () {
+    it("should be able to successfully subscribe to the channel and publish message to it on port 80", function (done) {
+        var pubnub = PUBNUB.init({
+          publish_key     : 'pub-c-82c13e30-f521-4419-b002-b8c99d5faa00',
+          subscribe_key   : 'sub-c-7756fcc2-9bd6-11e5-b0f3-02ee2ddab7fe',
+          ssl             : false,
+          origin          : origin,
+          uuid: uuid
+        });
 
-    it.only("should be able to successfully subscribe to the channel and publish message to it ", function (done) {
-
-      require('nock').recorder.rec();
-
-      var pubnub = PUBNUB.init({
-        publish_key: 'demo',
-        subscribe_key: 'demo',
-        ssl: false,
-        origin: origin
-      });
-
-      var subscribedChannel = channel + "_disabled_" + get_random();
-      nockHarness.mockPublish(subscribedChannel, encodeURI('"' + message + '"'), uuid, "ssl_test__send_requests_via_https.json");
-      nockHarness.mockSubscribe();
-
-      subscribeAndPublish(pubnub, subscribedChannel, done);
+        subscribeAndPublish(pubnub, channel + "_disabled_" + 10, done);
     });
 
     it("should send requests via HTTP to 80 port", function (done) {
-
       var pubnub = PUBNUB.init({
-        publish_key: 'demo',
-        subscribe_key: 'demo',
+        publish_key     : 'pub-c-82c13e30-f521-4419-b002-b8c99d5faa00',
+        subscribe_key   : 'sub-c-7756fcc2-9bd6-11e5-b0f3-02ee2ddab7fe',
         ssl: false,
         origin: origin,
         uuid: uuid
       });
 
-      nockHarness.mockPublish(channel, encodeURI('"' + message + '"'), uuid, "ssl_test__send_requests_via_http.json");
-
       pubnub.publish({
         channel: channel,
         message: message,
         callback: function () {
+          pubnub.shutdown();
           done();
         },
         error: function () {
+          pubnub.shutdown();
           done(new Error("Error callback triggered"));
         }
       });
     });
+
   });
 });
 
@@ -112,7 +103,12 @@ function subscribeAndPublish(pubnub, channel, done) {
     callback: function (msg, envelope, ch) {
       assert.equal(message, msg);
       assert.equal(channel, ch);
+      pubnub.shutdown();
       done();
+    },
+    error: function (err) {
+      pubnub.shutdown();
+      done(new Error("Error callback triggered"));
     }
   });
 }
